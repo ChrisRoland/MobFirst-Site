0aaf207b86372c2f5090b9c146861bd284401f2b
ChrisRoland <chrisebuberoland@gmail.com>
2023-05-05 21:10:03 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Animation for smooth scrolling
+$('#view-work').on('click', function() {
+    const images = $('#images').position().top;
+
+    $('html,body').animate(
+        {
+            scrollTop: images
+        }, 900
+    );
+});
